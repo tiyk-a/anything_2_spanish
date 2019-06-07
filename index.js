ab231258e566d924caa04c3bef956ade1132e505
tiyk-a <milejanaja@gmail.com>
2019-06-07 15:59:53 +0000

    [Test]Completely test looking for json data

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
           languageTranslator.translate(translateParams)
             .then(translationResult => {
               console.log(JSON.stringify(translationResult, null, 2));
-              console.log(translationResult.body."translations");
+              console.log(translationResult.translations[0].translation);
               // var translated = translationResult.body.translation
               // translated.push(translationResult);
               events_processed.push(bot.replyMessage(event.replyToken, {
