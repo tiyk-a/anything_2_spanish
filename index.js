1cc6bdc78c5ef70566b0f52f1bb503032c43a6f0
tiyk-a <milejanaja@gmail.com>
2019-06-08 06:28:56 +0000

    [Backup]Before large construction

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
           };
 
           languageTranslator.identify(identifyParams)
+          //  *********************************************************************************************************************
             .then(identifiedLanguages => {
               console.log(JSON.stringify(identifiedLanguages, null, 2));
               posted_lang.push(identifiedLanguages.languages[0].language);
@@ -104,26 +105,6 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
             // ;
             // IDENTIRY LANGUAGE BY IBM TRANSLATOR
 
-
-          //   // TRANSLATE BY IBM TRANSLATOR
-          //   var from_to = posted_lang[0] + '-es'
-          //   console.log(from_to)
-          //   const translateParams = {
-          //   text: event.message.text,
-          //   model_id: from_to,
-          //   };
-
-          // languageTranslator.translate(translateParams)
-
-          //   // TRANSACTION WHEN SUCCESSFULLY GOT TRANSLATION
-          //   .then(translationResult => {
-          //     console.log(JSON.stringify(translationResult, null, 2));
-          //     events_processed.push(bot.replyMessage(event.replyToken, {
-          //       type: "text",
-          //       text: translationResult.translations[0].translation
-          //     }));
-          //   })
-
             // TRANSACTION FOR WHRN ANY ERRORS HAPPENED
             .catch(err => {
               console.log('error:', err);
@@ -132,6 +113,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                 text: "There's no need of translation, mi amor solo quedate aqui...!"
               }));
             });
+            // ****************************************************************************************************************************
         }else if (event.type == "message" && event.message.type == "image"){
             events_processed.push(bot.replyMessage(event.replyToken, {
                 type: "text",
