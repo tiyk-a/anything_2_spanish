b9b19a35c297d96d204f8aad1b4b045929990d2b
tiyk-a <milejanaja@gmail.com>
2019-06-08 05:21:13 +0000

    [Update]Language identification is under construction

--- index.js
+++ index.js
@@ -28,38 +28,58 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
 
     // すべてのイベント処理のプロミスを格納する配列。
     let events_processed = [];
-    let translated = [];
 
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
+          // PREPARE EMPTY ARRAY TO PUT IDENTIDIED LANGUAGE CODE
+          let posted_lang = [];
 
+          // FOR AUTHOLIFICATION OF IBM TRANSLATOR
           const languageTranslator = new LanguageTranslatorV3({
             version: '2019-04-02',
             iam_apikey: process.env.IBM_KEY,
             url: 'https://gateway-tok.watsonplatform.net/language-translator/api',
           });
 
-          const translateParams = {
+          // IDENTIRY LANGUAGE BY IBM TRANSLATOR
+          const identifyParams = {
+            text: event.message.text
+          };
+
+          languageTranslator.identify(identifyParams)
+          .then(identifiedLanguages => {
+            console.log(JSON.stringify(identifiedLanguages, null, 2));
+            posted_lang.push(identifiedLanguages.languages[0].language);
+          })
+          .catch(err => {
+            console.log('error:', err);
+            posted_lang.push('en');
+          });
+          // IDENTIRY LANGUAGE BY IBM TRANSLATOR
+
+          // TRANSLATED BY IBM TRANSLATOR
+          var from_to = posted_lang[0] + '-es'
+            const translateParams = {
             text: event.message.text,
-            model_id: 'en-es',
+            model_id: from_to,
           };
 
           languageTranslator.translate(translateParams)
+
+            // TRANSACTION WHEN SUCCESSFULLY GOT TRANSLATION
             .then(translationResult => {
               console.log(JSON.stringify(translationResult, null, 2));
-              console.log(translationResult.translations[0].translation);
-              // var translated = translationResult.body.translation
-              // translated.push(translationResult);
               events_processed.push(bot.replyMessage(event.replyToken, {
                 type: "text",
                 text: translationResult.translations[0].translation
               }));
             })
+
+            // TRANSACTION FOR WHRN ANY ERRORS HAPPENED
             .catch(err => {
               console.log('error:', err);
-              // translated.push("There's no need of translation, mi amor solo quedate aqui...!");
               events_processed.push(bot.replyMessage(event.replyToken, {
                 type: "text",
                 text: "There's no need of translation, mi amor solo quedate aqui...!"
